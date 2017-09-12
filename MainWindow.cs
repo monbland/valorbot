3d83ebe68e84685209e2b3c1a6015f5710ddd108
moonbland <mr.erikost@gmail.com>
2017-09-12 17:22:36 +0300

    Начальный возврат модуля Valoratk

---
+++ MainWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using TeleSharp.TL;
+using TeleSharp.TL.Messages;
+using TLSharp.Core;
+using TLSharp.Core.Network;
+using TLSharp.Core.Requests;
+using TLSharp.Core.Utils;
+
+public partial class MainWindow : Gtk.Window
+{
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+}
